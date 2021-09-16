f48f25bed0f6d32cc60284c4caa7866e9e9d8388
Thiago Resende <thiago.gcresende@gmail.com>
2021-09-16 19:38:58 -0300

    chore: add dependencies

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/tests/e2e/cypress'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+}
---
+++ src/a.spec.ts
@@ -0,0 +1,5 @@
+describe('a', () => {
+  test('should ', () => {
+    expect(1).toBe(1)
+  })
+})
