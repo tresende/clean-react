4f72c26869059f39601262fc1c91af600187df55
Thiago Resende <thiago.gcresende@gmail.com>
2021-09-18 12:32:43 -0300

    feat: add authentication stuff

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,13 @@
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { RemoteAuthentication } from './remote-authentication'
+
+describe('RemoteAuthentication ', () => {
+  test('Should call HttpPostClient with correct URL', async () => {
+    //System Under Test
+    const url = 'FAKE_URL'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    await sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
