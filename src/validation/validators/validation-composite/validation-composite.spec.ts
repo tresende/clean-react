d7201389fe097ba7adc411052c6966556758115d
Thiago Resende <thiago.gcresende@gmail.com>
2021-10-15 15:18:25 -0300

    refactor: change tests folder

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,7 +1,7 @@
 import faker from 'faker'
 
-import { FieldValidationSpy } from '../test/mock-field-validation'
-import { ValidationComposite } from './validation-composite'
+import { FieldValidationSpy } from '@/validation/test'
+import { ValidationComposite } from '@/validation/validators'
 
 type SutTypes = {
   sut: ValidationComposite
